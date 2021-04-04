bdd716e22cc65e3aa73d083b55a2cc7784fecf22
buck <641965812@qq.com>
2021-04-04 23:25:26 +0800

    第三周提交作业

---
+++ week03/HttpHandler.java
@@ -0,0 +1,97 @@
+package java0.nio01.netty;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import io.netty.util.ReferenceCountUtil;
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONNECTION;
+import static io.netty.handler.codec.http.HttpHeaderValues.KEEP_ALIVE;
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+import java.io.IOException;
+
+public class HttpHandler extends ChannelInboundHandlerAdapter {
+    
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        try {
+            //logger.info("channelRead流量接口请求开始，时间为{}", startTime);
+            FullHttpRequest fullRequest = (FullHttpRequest) msg;
+            String uri = fullRequest.uri();
+            fullRequest.headers().set("xjava", "soul");
+            //logger.info("接收到的请求url为{}", uri);
+            if (uri.contains("/test")) {
+                handlerTest(fullRequest, ctx);
+            }
+    
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    private void handlerTest(FullHttpRequest fullRequest, ChannelHandlerContext ctx) {
+        FullHttpResponse response = null;
+        try {
+        	String value = getServer01Message(fullRequest); // "hello,kimmking"; // 对接上次作业的httpclient或者okhttp请求另一个url的响应数据
+
+//            httpGet ...  http://localhost:8801
+//            返回的响应，"hello,nio";
+//            value = reponse....
+
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(value.getBytes("UTF-8")));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", response.content().readableBytes());
+            response.headers().set("soul","xjava");
+
+        } catch (Exception e) {
+            System.out.println("处理出错:"+e.getMessage());
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    response.headers().set(CONNECTION, KEEP_ALIVE);
+                    ctx.write(response);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+    /**
+     * 按照/test路径请求server01信息,并通过网关添加xjava请求头进行过滤
+     * @return
+     * @throws IOException
+     */
+    public String getServer01Message(FullHttpRequest fullRequest) throws IOException {
+    	OkHttpClient client = new OkHttpClient();
+		Request request = new Request.Builder().url("http://localhost:8801").header("xjava", fullRequest.headers().get("xjava"))// 要访问的链接
+				.build();
+	    return client.newCall(request).execute().body().string(); 
+    }
+
+}
